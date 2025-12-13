a7259309a91a28e1b769d53030671ed00581d814
Taswell <taswell@ecomplete.co.za>
2025-12-13 13:28:16 +0200

    Update config.ts
    api

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "shipment-5b687",
   "appId": "1:269674227376:web:bea1d2089e1f2368363d75",
-  "apiKey": "AIzaSyAv_zxfYRSLNbUsybsTz_7PclruqI3MvsM",
+  "apiKey": "x",
   "authDomain": "shipment-5b687.firebaseapp.com",
   "storageBucket": "shipment-5b687.appspot.com",
   "measurementId": "",
