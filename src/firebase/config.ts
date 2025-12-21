fd482762821cfdfd0643193e308648bbb5e7f39f
Taswell Solomons <taswell@ecomplete.co.za>
2025-12-21 13:42:35 +0000

    rather set api back to AIzaSyCvZVJDB2GeMlopwGj7phDZhooJwiZP7Iw

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "shipment-5b687",
   "appId": "1:269674227376:web:bea1d2089e1f2368363d75",
-  "apiKey": "AIzaSyCYnLc7jgZekC5ztSCu_UZcoObU5hE5dZs",
+  "apiKey": "AIzaSyCvZVJDB2GeMlopwGj7phDZhooJwiZP7Iw",
   "authDomain": "shipment-5b687.firebaseapp.com",
   "storageBucket": "shipment-5b687.appspot.com",
   "measurementId": "",
