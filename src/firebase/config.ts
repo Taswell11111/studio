a93d047bdf58bf0d38e4e943c4a049f89c4ea20e
Taswell Solomons <taswell@ecomplete.co.za>
2025-12-21 12:54:15 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/api

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "shipment-5b687",
   "appId": "1:269674227376:web:bea1d2089e1f2368363d75",
-  "apiKey": "AIzaSyC1fio-G-95H-931KYSy1c4p8v25w7s3Hw",
+  "apiKey": "AIzaSyA_I-g_1s_2-3_4_5-6_7_8-9_0-abc-def",
   "authDomain": "shipment-5b687.firebaseapp.com",
   "storageBucket": "shipment-5b687.appspot.com",
   "measurementId": "",
