5c3da96fd59bb514fa24bd93039bc52a685af3ba
Taswell Solomons <taswell@ecomplete.co.za>
2025-12-21 13:36:34 +0000

    New API: AIzaSyCYnLc7jgZekC5ztSCu_UZcoObU5hE5dZs

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "shipment-5b687",
   "appId": "1:269674227376:web:bea1d2089e1f2368363d75",
-  "apiKey": "AIzaSyDk8t5V6r_GTVGse_2G92kS7G_3z1pZ68Q",
+  "apiKey": "AIzaSyCYnLc7jgZekC5ztSCu_UZcoObU5hE5dZs",
   "authDomain": "shipment-5b687.firebaseapp.com",
   "storageBucket": "shipment-5b687.appspot.com",
   "measurementId": "",
